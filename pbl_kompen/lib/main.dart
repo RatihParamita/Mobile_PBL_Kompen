1893cca34c93bf4d347e84b812a8804d205ee80a
Ratih Paramita <significantratih@gmail.com>
2024-09-29 23:23:49 +0700

    MHS Homepage Hutang

--- pbl_kompen/lib/main.dart
+++ pbl_kompen/lib/main.dart
@@ -1,125 +1,219 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp()); // Menjalankan aplikasi Flutter kita
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false, // Menghilangkan banner "Debug" di atas
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
+        fontFamily:
+            'InstrumentSans-Bold', // Menetapkan font default menjadi Instrument Sans
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home:
+          MhsHomepageHutang(), // Menyatakan bahwa halaman utama adalah MhsHomepageHutang
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
+class MhsHomepageHutang extends StatefulWidget {
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  _MhsHomepageHutangState createState() => _MhsHomepageHutangState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+class _MhsHomepageHutangState extends State<MhsHomepageHutang> {
+  int _selectedIndex =
+      0; // Variabel untuk menyimpan index dari item yang dipilih
 
-  void _incrementCounter() {
+  // Fungsi untuk mengupdate state saat item di bottom navigation dipilih
+  void _onItemTapped(int index) {
     setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
+      _selectedIndex = index;
     });
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+        backgroundColor:
+            const Color.fromARGB(255, 45, 27, 107), // Warna AppBar biru tua
+        title: RichText(
+          text: TextSpan(
+            style: TextStyle(
+              fontFamily: 'InstrumentSans',
+              fontSize: 24,
+              fontWeight: FontWeight.bold,
+              color: Colors.white, // Warna teks default
+            ),
+            children: <TextSpan>[
+              TextSpan(text: 'Sistem Kompensasi '), // Teks biasa
+              TextSpan(
+                  text: 'J',
+                  style:
+                      TextStyle(color: const Color.fromARGB(255, 153, 58, 54))),
+              TextSpan(
+                  text: 'T',
+                  style:
+                      TextStyle(color: const Color.fromARGB(255, 240, 85, 41))),
+              TextSpan(
+                  text: 'I',
+                  style: TextStyle(
+                      color: const Color.fromARGB(255, 254, 192, 26))),
+              TextSpan(text: ' Polinema'), // Teks biasa
+            ],
+          ),
+        ), // Judul di AppBar
+        centerTitle: true, // Membuat judul berada di tengah
+        actions: [
+          IconButton(
+            icon:
+                Icon(Icons.notifications_outlined, // Ikon lonceng di kanan atas
+                    color: Colors.white), // warna ikon lonceng
+            onPressed: () {
+              // Aksi jika ikon notifikasi diklik (belum ada fungsi)
+            },
+          ),
+        ],
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
+      body: SingleChildScrollView(
+        // Agar halaman bisa di-scroll
+        padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
         child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+          crossAxisAlignment:
+              CrossAxisAlignment.center, // Pusatkan elemen-elemen
+          children: [
+            SizedBox(height: 5), // Memberi jarak
+            Text(
+              'Selamat datang,', // Teks selamat datang
+              style: TextStyle(
+                fontFamily: 'InstrumentSans',
+                fontSize: 21,
+                fontWeight: FontWeight.bold, // Teks lebih tebal
+              ),
             ),
             Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
+              'NAMA MAHASISWA', // Nama mahasiswa
+              style: TextStyle(
+                fontFamily: 'InstrumentSans',
+                fontSize: 24,
+                fontWeight: FontWeight.bold, // Teks lebih tebal
+              ),
+            ),
+            SizedBox(height: 20), // Jarak antara elemen
+            Image(
+              image: AssetImage('assets/images/logo_polinema.png'),
+              height: 150, // Tinggi gambar logo
+              width: 149, // Lebar gambar logo
+            ),
+            SizedBox(height: 20),
+            _buildTextField('Total Absensi Alfa',
+                'Total jam'), // Menampilkan field total absen
+            SizedBox(height: 5),
+            _buildTextField('Jumlah Absensi Alfa Lunas',
+                'Jumlah jam'), // Menampilkan field absen lunas
+            SizedBox(height: 5),
+            _buildTextField('Jumlah Absensi Alfa Belum Lunas',
+                'Jumlah jam'), // Field absen belum lunas
+            SizedBox(height: 5),
+            Align(
+              alignment: Alignment.centerLeft,
+              child: Text(
+                'Status Ujian Akhir Semester (UAS)', // Teks status UAS
+                style: TextStyle(fontSize: 16),
+              ),
+            ),
+            SizedBox(height: 5),
+            Container(
+              width: double.infinity, // Membuat lebar sesuai layar
+              padding: EdgeInsets.all(15),
+              decoration: BoxDecoration(
+                color: Colors.red, // Warna kotak merah
+                border:
+                    Border.all(color: const Color.fromARGB(255, 144, 11, 9)),
+                borderRadius:
+                    BorderRadius.circular(8), // Sudut kotak melengkung
+              ),
+              child: Text(
+                'Belum bisa mengikuti!', // Teks di dalam kotak merah
+                style: TextStyle(
+                  color:
+                      const Color.fromARGB(255, 144, 11, 9), // Teks warna merah
+                  fontSize: 16,
+                  fontWeight: FontWeight.normal, // Teks normal
+                ),
+                textAlign: TextAlign.left, // Teks di kiri
+              ),
             ),
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+
+      // BottomNavigationBar
+      bottomNavigationBar: BottomNavigationBar(
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: 'Beranda',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.menu),
+            label: 'Datar Request',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.cases_outlined),
+            label: 'Daftar Tugas',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.person),
+            label: 'Profil',
+          ),
+        ],
+        currentIndex: _selectedIndex, // Index yang sedang dipilih
+        selectedItemColor: Colors.white, // Warna item terpilih
+        unselectedItemColor: Colors.white, // Warna item tidak terpilih
+        backgroundColor: Color.fromARGB(
+            255, 45, 27, 107), // Warna latar belakang BottomNavigationBar
+        onTap: _onItemTapped, // Aksi ketika item dipilih
+        type: BottomNavigationBarType
+            .fixed, // Menetapkan jenis BottomNavigationBar
+      ),
+    );
+  }
+
+  // Fungsi untuk membuat TextField dengan label dan hint
+  Widget _buildTextField(String label, String hint) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Text(
+          label, // Label yang muncul di atas kotak input
+          style: TextStyle(fontSize: 16),
+        ),
+        SizedBox(height: 5),
+        TextField(
+          decoration: InputDecoration(
+            hintText: hint, // Teks yang muncul di dalam kotak
+            hintStyle: TextStyle(
+              color: Colors
+                  .grey, // Warna teks abu-abu pada "Total jam" dan "Jumlah jam"
+            ),
+            border: OutlineInputBorder(
+              borderRadius: BorderRadius.circular(8), // Sudut kotak melengkung
+              borderSide: BorderSide(
+                color: Colors.grey
+                    .withOpacity(0.1), // Warna border dengan opacity 50%
+                width: 1, // Lebar garis border
+              ),
+            ),
+            contentPadding: EdgeInsets.symmetric(horizontal: 10, vertical: 10),
+          ),
+          readOnly: true, // Membuat input hanya untuk dibaca, tidak bisa diubah
+        ),
+      ],
     );
   }
 }
