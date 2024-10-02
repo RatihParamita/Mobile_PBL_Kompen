d3ad0c016aa83fab1971008b04d221a555363726
Ratih Paramita <significantratih@gmail.com>
2024-10-02 19:14:11 +0700

    MHS Homepage Hutang

--- pbl_kompen/lib/main.dart
+++ pbl_kompen/lib/main.dart
@@ -1,10 +1,12 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MyApp()); // Menjalankan aplikasi Flutter kita
+  runApp(const MyApp()); // Menjalankan aplikasi Flutter kita
 }
 
 class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -14,12 +16,14 @@ class MyApp extends StatelessWidget {
             'InstrumentSans-Bold', // Menetapkan font default menjadi Instrument Sans
       ),
       home:
-          MhsHomepageHutang(), // Menyatakan bahwa halaman utama adalah MhsHomepageHutang
+          const MhsHomepageHutang(), // Menyatakan bahwa halaman utama adalah MhsHomepageHutang
     );
   }
 }
 
 class MhsHomepageHutang extends StatefulWidget {
+  const MhsHomepageHutang({super.key});
+
   @override
   _MhsHomepageHutangState createState() => _MhsHomepageHutangState();
 }
@@ -41,118 +45,119 @@ class _MhsHomepageHutangState extends State<MhsHomepageHutang> {
       appBar: AppBar(
         backgroundColor:
             const Color.fromARGB(255, 45, 27, 107), // Warna AppBar biru tua
-        title: RichText(
-          text: TextSpan(
-            style: TextStyle(
-              fontFamily: 'InstrumentSans',
-              fontSize: 24,
-              fontWeight: FontWeight.bold,
-              color: Colors.white, // Warna teks default
-            ),
-            children: <TextSpan>[
-              TextSpan(text: 'Sistem Kompensasi '), // Teks biasa
-              TextSpan(
-                  text: 'J',
-                  style:
-                      TextStyle(color: const Color.fromARGB(255, 153, 58, 54))),
-              TextSpan(
-                  text: 'T',
-                  style:
-                      TextStyle(color: const Color.fromARGB(255, 240, 85, 41))),
-              TextSpan(
-                  text: 'I',
+        title: Stack(
+          children: [
+            Align(
+              alignment: Alignment.center, // Pusatkan teks di tengah
+              child: RichText(
+                textAlign: TextAlign.center, // Center alignment untuk teks
+                text: const TextSpan(
                   style: TextStyle(
-                      color: const Color.fromARGB(255, 254, 192, 26))),
-              TextSpan(text: ' Polinema'), // Teks biasa
-            ],
-          ),
-        ), // Judul di AppBar
+                    fontFamily: 'InstrumentSans',
+                    fontSize: 24,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.white, // Warna teks default
+                  ),
+                  children: <TextSpan>[
+                    TextSpan(text: 'Sistem Kompensasi\n'), // Teks biasa
+                    TextSpan(
+                        text: 'J',
+                        style:
+                            TextStyle(color: Color.fromARGB(255, 153, 58, 54))),
+                    TextSpan(
+                        text: 'T',
+                        style:
+                            TextStyle(color: Color.fromARGB(255, 240, 85, 41))),
+                    TextSpan(
+                        text: 'I',
+                        style: TextStyle(
+                            color: Color.fromARGB(255, 254, 192, 26))),
+                    TextSpan(text: ' Polinema'), // Teks biasa
+                  ],
+                ),
+              ),
+            ),
+            Align(
+              alignment: Alignment.centerRight, // Tempatkan ikon di kanan
+              child: IconButton(
+                icon: const Icon(
+                  Icons.notifications_outlined, // Ikon lonceng
+                  color: Colors.white,
+                ),
+                onPressed: () {
+                  // Aksi jika ikon notifikasi diklik
+                },
+              ),
+            ),
+          ],
+        ),
         centerTitle: true, // Membuat judul berada di tengah
-        actions: [
-          IconButton(
-            icon:
-                Icon(Icons.notifications_outlined, // Ikon lonceng di kanan atas
-                    color: Colors.white), // warna ikon lonceng
-            onPressed: () {
-              // Aksi jika ikon notifikasi diklik (belum ada fungsi)
-            },
-          ),
-        ],
       ),
       body: SingleChildScrollView(
-        // Agar halaman bisa di-scroll
         padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
         child: Column(
-          crossAxisAlignment:
-              CrossAxisAlignment.center, // Pusatkan elemen-elemen
+          crossAxisAlignment: CrossAxisAlignment.center,
           children: [
-            SizedBox(height: 5), // Memberi jarak
-            Text(
-              'Selamat datang,', // Teks selamat datang
+            const SizedBox(height: 5),
+            const Text(
+              'Selamat datang,',
               style: TextStyle(
                 fontFamily: 'InstrumentSans',
                 fontSize: 21,
-                fontWeight: FontWeight.bold, // Teks lebih tebal
+                fontWeight: FontWeight.bold,
               ),
             ),
-            Text(
-              'NAMA MAHASISWA', // Nama mahasiswa
+            const Text(
+              'NAMA MAHASISWA',
               style: TextStyle(
                 fontFamily: 'InstrumentSans',
                 fontSize: 24,
-                fontWeight: FontWeight.bold, // Teks lebih tebal
+                fontWeight: FontWeight.bold,
               ),
             ),
-            SizedBox(height: 20), // Jarak antara elemen
-            Image(
+            const SizedBox(height: 20),
+            const Image(
               image: AssetImage('assets/images/logo_polinema.png'),
-              height: 150, // Tinggi gambar logo
-              width: 149, // Lebar gambar logo
+              height: 150,
+              width: 149,
             ),
-            SizedBox(height: 20),
-            _buildTextField('Total Absensi Alfa',
-                'Total jam'), // Menampilkan field total absen
-            SizedBox(height: 5),
-            _buildTextField('Jumlah Absensi Alfa Lunas',
-                'Jumlah jam'), // Menampilkan field absen lunas
-            SizedBox(height: 5),
-            _buildTextField('Jumlah Absensi Alfa Belum Lunas',
-                'Jumlah jam'), // Field absen belum lunas
-            SizedBox(height: 5),
-            Align(
+            const SizedBox(height: 20),
+            _buildTextField('Total Absensi Alfa', 'Total jam'),
+            const SizedBox(height: 5),
+            _buildTextField('Jumlah Absensi Alfa Lunas', 'Jumlah jam'),
+            const SizedBox(height: 5),
+            _buildTextField('Jumlah Absensi Alfa Belum Lunas', 'Jumlah jam'),
+            const SizedBox(height: 5),
+            const Align(
               alignment: Alignment.centerLeft,
               child: Text(
-                'Status Ujian Akhir Semester (UAS)', // Teks status UAS
+                'Status Ujian Akhir Semester (UAS)',
                 style: TextStyle(fontSize: 16),
               ),
             ),
-            SizedBox(height: 5),
+            const SizedBox(height: 5),
             Container(
-              width: double.infinity, // Membuat lebar sesuai layar
-              padding: EdgeInsets.all(15),
+              width: double.infinity,
+              padding: const EdgeInsets.all(15),
               decoration: BoxDecoration(
-                color: Colors.red, // Warna kotak merah
+                color: Colors.red,
                 border:
                     Border.all(color: const Color.fromARGB(255, 144, 11, 9)),
-                borderRadius:
-                    BorderRadius.circular(8), // Sudut kotak melengkung
+                borderRadius: BorderRadius.circular(8),
               ),
-              child: Text(
-                'Belum bisa mengikuti!', // Teks di dalam kotak merah
+              child: const Text(
+                'Belum bisa mengikuti!',
                 style: TextStyle(
-                  color:
-                      const Color.fromARGB(255, 144, 11, 9), // Teks warna merah
+                  color: Color.fromARGB(255, 144, 11, 9),
                   fontSize: 16,
-                  fontWeight: FontWeight.normal, // Teks normal
+                  fontWeight: FontWeight.normal,
                 ),
-                textAlign: TextAlign.left, // Teks di kiri
+                textAlign: TextAlign.left,
               ),
             ),
           ],
         ),
       ),
-
-      // BottomNavigationBar
       bottomNavigationBar: BottomNavigationBar(
         items: const <BottomNavigationBarItem>[
           BottomNavigationBarItem(
@@ -172,14 +177,12 @@ class _MhsHomepageHutangState extends State<MhsHomepageHutang> {
             label: 'Profil',
           ),
         ],
-        currentIndex: _selectedIndex, // Index yang sedang dipilih
-        selectedItemColor: Colors.white, // Warna item terpilih
-        unselectedItemColor: Colors.white, // Warna item tidak terpilih
-        backgroundColor: Color.fromARGB(
-            255, 45, 27, 107), // Warna latar belakang BottomNavigationBar
-        onTap: _onItemTapped, // Aksi ketika item dipilih
-        type: BottomNavigationBarType
-            .fixed, // Menetapkan jenis BottomNavigationBar
+        currentIndex: _selectedIndex,
+        selectedItemColor: Colors.white,
+        unselectedItemColor: Colors.white,
+        backgroundColor: const Color.fromARGB(255, 45, 27, 107),
+        onTap: _onItemTapped,
+        type: BottomNavigationBarType.fixed,
       ),
     );
   }
@@ -191,13 +194,13 @@ class _MhsHomepageHutangState extends State<MhsHomepageHutang> {
       children: [
         Text(
           label, // Label yang muncul di atas kotak input
-          style: TextStyle(fontSize: 16),
+          style: const TextStyle(fontSize: 16),
         ),
-        SizedBox(height: 5),
+        const SizedBox(height: 5),
         TextField(
           decoration: InputDecoration(
             hintText: hint, // Teks yang muncul di dalam kotak
-            hintStyle: TextStyle(
+            hintStyle: const TextStyle(
               color: Colors
                   .grey, // Warna teks abu-abu pada "Total jam" dan "Jumlah jam"
             ),
@@ -209,7 +212,8 @@ class _MhsHomepageHutangState extends State<MhsHomepageHutang> {
                 width: 1, // Lebar garis border
               ),
             ),
-            contentPadding: EdgeInsets.symmetric(horizontal: 10, vertical: 10),
+            contentPadding:
+                const EdgeInsets.symmetric(horizontal: 10, vertical: 10),
           ),
           readOnly: true, // Membuat input hanya untuk dibaca, tidak bisa diubah
         ),
